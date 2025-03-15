0a14bbcc87153e10aa0eec61cc695aee929ad231
artem.degtyarev <artem.d@rutorika.ru>
2025-03-15 13:32:04 +0300

    update basis transcoder path

--- src/hooks/useKTX2Loader.js
+++ src/hooks/useKTX2Loader.js
@@ -4,12 +4,12 @@ import { REVISION } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { MeshoptDecoder } from 'three-stdlib';
 
-const THREE_PATH = `https://unpkg.com/three@0.${REVISION}.x`;
-
 export const useKTX2Loader = () => {
   const { gl } = useThree();
   const ktx2Loader = new KTX2Loader();
-  ktx2Loader.setTranscoderPath(`${THREE_PATH}/examples/jsm/libs/basis/`);
+  // const basisPath = `https://cdn.jsdelivr.net/npm/three@0.${THREE.REVISION}.x/examples/jsm/libs/basis/`;
+  const basisPath = '/libs/basis/';
+  ktx2Loader.setTranscoderPath(basisPath);
   ktx2Loader.detectSupport(gl);
 
   const loadTexture = (textureUrl, callback) => {
